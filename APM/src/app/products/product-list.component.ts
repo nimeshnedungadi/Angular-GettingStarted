bf990a8f2e1c59bb2551ab720b0b4a2805a88025
Nimesh Nedungadi <nined@microsoft.com>
2019-10-08 19:21:29 -0700

    Using http

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string = '';
     _listFilter: string;
     get listFilter(): string {
         return this._listFilter;
@@ -29,8 +30,13 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
     }
 
     toggleImage(): void {
